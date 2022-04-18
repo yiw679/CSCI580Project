a0dfe608c068a156d1a0c74273f1467c258d5769
Han Chen <hellohanchen@gmail.com>
2022-04-17 19:19:57 -0700

    Shader with single light
    Support lights in the shader

--- Real Scene Rendering and Interaction/Source/Shader.h
+++ Real Scene Rendering and Interaction/Source/Shader.h
@@ -23,6 +23,8 @@ public:
 
     void setMat4(const std::string& name, const glm::mat4& mat) const;
 
+    void setVec4(const std::string& name, const GLfloat* value) const;
+
     void setFloat(const std::string& name, float value) const;
 
     void checkCompileErrors(GLuint shader, std::string type);
